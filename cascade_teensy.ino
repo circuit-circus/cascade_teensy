e5b3c1ca873af413d63a20a8b67c1959a019d0d0
CSivertsen <chr@csiv.dk>
2019-04-17 15:02:01 +0200

    Committing before switching to WS2812Serial

--- cascade_teensy.ino
+++ cascade_teensy.ino
@@ -23,13 +23,13 @@
     pin 21: LED strip #7
     pin 5:  LED strip #8
 */
-
+#define FASTLED_ALLOW_INTERRUPTS 0 //If the amount of LEDs is above approx 75 the LEDs start flickering when interrupts are on. 
 #include <FastLED.h>
 #define COLOR_ORDER GRB
 #define CHIPSET     WS2812
 #define BRIGHTNESS  200
-#define FRAMES_PER_SECOND 60
-#define TOTAL_LED_COUNT 21 //You have to manually add the total LED count here
+//#define FRAMES_PER_SECOND 60
+#define TOTAL_LED_COUNT 84 //You have to manually add the total LED count here
 
 /*  Two dimensional array containing the physical arrangement of the LED.
     The first array contains one array for each circle. In Cascade will be 4.
@@ -37,7 +37,7 @@
 */
 
 const uint8_t numStrips = 1;
-const uint8_t stripLengths[] = {21};
+const uint8_t stripLengths[] = {TOTAL_LED_COUNT};
 
 CRGB leds[TOTAL_LED_COUNT];
 
@@ -45,6 +45,10 @@ uint8_t currentDisplay; // Used to keep track of which circle we are receiving d
 const uint8_t numSensors = 4;
 //uint16_t sensorData[numSensors];
 uint16_t sensorData[] = {4, 3, 2, 1};
+boolean firstContact = false;
+int serialCount;
+boolean isReady = true;
+DMAMEM int ledData[TOTAL_LED_COUNT * 3 + numStrips];
 
 
 void setup() {
@@ -61,59 +65,166 @@ void setup() {
   }
   FastLED.show();
 
-  establishContact();  // send a byte to establish contact until Processing responds
+  //establishContact();  // send a byte to establish contact until Processing responds
 
 }
 
-void establishContact() {
-  while (Serial.available() <= 0) {
-    Serial.write('A');   // send a capital A
-    delay(300);
-  }
-}
+//void establishContact() {
+//  while (Serial.available() <= 0) {
+//    Serial.write('A');   // send a capital A
+//    delay(300);
+//  }
+//}
 
 //This function can be changed to fit any kind and amount of sensors
 void readSensors() {
 
 }
 
+//Reads one incoming byte at a time
+//void loop() {
+//  readSensors();
+//  Serial.write('!');
+//  for (int i = 0; i < numSensors; i++) {
+//    Serial.write(sensorData[i]);
+//  }
+//  if (Serial.available() > 0) {
+//    int inByte = Serial.read();
+//    if (serialCount == 0) {
+//      currentDisplay = inByte;
+//    } else {
+//      ledData[serialCount - 1] = inByte;
+//      sensorData[0]++;
+//    }
+//    serialCount++;
+//
+//    if (serialCount >= TOTAL_LED_COUNT * 3 + 1) {
+//      for (int i = 0; i < TOTAL_LED_COUNT; i++) {
+//        int index = i * 3; //Index adjusted to 3 colors and 1 display ID
+//        int r = ledData[index];
+//        int g = ledData[index + 1];
+//        int b = ledData[index + 2];
+//        leds[i] = CRGB(r, g, b);
+//
+//        Serial.write('#'); //Request new led data
+//        FastLED.show();
+//        serialCount = 0;
+//        sensorData[0] = ledData[0];
+//      }
+//    }
+//  }
+//  else {
+//    Serial.write('#');
+//
+//  }
+//}
+
+//Reads all incoming bytes at once
 void loop() {
+  readSensors();
+  Serial.write('!');
+  for (int i = 0; i < numSensors; i++) {
+    Serial.write(sensorData[i]);
+  }
   if (Serial.available() > 0) {
-    for (int i = 0; i < numSensors; i++) {
-      readSensors();
-      Serial.write(sensorData[i]);
-    }
-    
-    // get incoming byte:
-    int startChar = Serial.read();
-    if (startChar == 'S') {
-      byte ledData[TOTAL_LED_COUNT * 3 + 1];
-      int count = Serial.readBytesUntil('!', ledData, TOTAL_LED_COUNT * 3 + 1 + 1); //Number of leds * 3 colors + 1 display ID + 1 termination character
-      if (count == TOTAL_LED_COUNT * 3 + 1 + 1) {
-        currentDisplay = ledData[0];
-        for (int i = 0; i < TOTAL_LED_COUNT; i++) {
-          int index = i * 3 + 1; //Index adjusted to 3 colors and 1 display ID
-          int r = ledData[index];
-          int g = ledData[index + 1];
-          int b = ledData[index + 2];
-          leds[i] = CRGB(r, g, b);
+    serialCount = Serial.readBytes((char *)ledData, TOTAL_LED_COUNT * 3 + numStrips); //Number of leds * 3 colors + 1 display ID 
+    if (serialCount >= TOTAL_LED_COUNT * 3 + 1) {
+      for (int i = 0; i < TOTAL_LED_COUNT; i++) {
+        int index = i * 3; //Index adjusted to 3 colors and 1 display ID
+        int r = ledData[index];
+        int g = ledData[index + 1];
+        int b = ledData[index + 2];
+        leds[i] = CRGB(r, g, b);
 
-          //Send sensor data back
-          if (i == TOTAL_LED_COUNT - 1 ) {
-            sensorData[0] = r;
-            sensorData[1] = g;
-            sensorData[2] = b;
-            sensorData[3] = count;
-          }
-          FastLED.show();
-
-
- 
-         //Serial.write('A'); //Triggers Processings serialEvent
-        }
+        Serial.write('#'); //Request new led data
+        FastLED.show();
+        sensorData[0] = serialCount;
+        serialCount = 0;
+        
       }
     }
+  }
+  else {
+    Serial.write('#');
 
   }
 }
+
+
+
+//Reads all incoming bytes at once
+//void loop() {
+//  if (Serial.available() > 0) {
+//    Serial.write('A');
+//    firstcontact
+//    int startChar = Serial.read();
+//    if (startChar == 'A') {
+//      for (int i = 0; i < numSensors; i++) {
+//        readSensors();
+//        Serial.write(sensorData[i]);
+//      }
+//    }
+//    if (startChar == 'S') {
+//        byte ledData[TOTAL_LED_COUNT * 3 + 1];
+//        int count = Serial.readBytes(ledData, TOTAL_LED_COUNT * 3 + 1 + 1); //Number of leds * 3 colors + 1 display ID + 1 termination character
+//        if (count == TOTAL_LED_COUNT * 3 + 1 + 1) {
+//          currentDisplay = ledData[0];
+//          for (int i = 0; i < TOTAL_LED_COUNT; i++) {
+//            int index = i * 3 + 1; //Index adjusted to 3 colors and 1 display ID
+//            int r = ledData[index];
+//            int g = ledData[index + 1];
+//            int b = ledData[index + 2];
+//            leds[i] = CRGB(r, g, b);
+//
+//            FastLED.show();
+//            //Serial.write('A'); //Triggers Processings serialEvent
+//          }
+//        }
+//      }
+//
+//  }
+//}
+
+//void serialSend() {
+//
+//  //Send sensor data back
+//  if (i == TOTAL_LED_COUNT - 1 ) {
+//    sensorData[0] = r;
+//    sensorData[1] = g;
+//    sensorData[2] = b;
+//    sensorData[3] = count;
+//  }
+//}
+
+//// Read information from serial connection
+//void serialEvent() {
+//  int inByte = Serial.read();
+//
+//  // Establish contact
+//  if (firstContact == false) {
+//    if (inByte == 'A') {
+//      Serial.flush();
+//      firstContact = true;
+//      Serial.write('A');
+//    }
+//    // If contact is already established store the data the arrays
+//  } else {
+//    currentDisplay = ledData[0];
+//    //for (int i = 0; i < TOTAL_LED_COUNT; i++)
+//    int index = serialCount * 3 + 1; //Index adjusted to 3 colors and 1 display ID
+//    int r = ledData[index];
+//    int g = ledData[index + 1];
+//    int b = ledData[index + 2];
+//    leds[serialCount] = CRGB(r, g, b);
+//    serialCount++;
+//
+//    // Send an A when all data has been received.
+//    if (serialCount > TOTAL_LED_COUNT * 3 + 1) {
+//      Serial.write('A');
+//      serialCount = 0;
+//      FastLED.show();
+//    }
+//
+//  }
+//}
 
